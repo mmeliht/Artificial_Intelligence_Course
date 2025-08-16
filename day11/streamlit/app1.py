898d5898bdb03419b0165a884d9367097a4a78a3
M.Melih TÜFEKCİOĞLU <melihtufekcioglu@gmail.com>
2025-08-16 14:06:20 +0300

    video import in web site with flask

--- day11/streamlit/app1.py
+++ day11/streamlit/app1.py
@@ -1,3 +1,7 @@
 import streamlit as st
 
-st.title("Tek Kelimeyle Web Sites")
+st.title("MLOps Spreamlit Apps :flag-tr:") # :flag-tr: yerine iconun adını yazarsak o gelir
+
+st.video("data/secret_of_success.mp4")
+
+st.image("data/image_01.jpg")
