99f1bff9539fc740d4bc79a46903da072bd9270a
M.Melih TÜFEKCİOĞLU <melihtufekcioglu@gmail.com>
2025-08-16 12:04:37 +0300

    maas tahmin wweb site and api created

--- day11/app.py
+++ day11/app.py
@@ -1,6 +1,18 @@
-from flask import Flask , render_template , request
+from flask import Flask, render_template, request
 import pickle
+app=Flask(__name__)
+model=pickle.load(open("maas.pkl","rb"))
+@app.route("/")
+def index():
+    return render_template("index.html")
 
-app = Flask(__main__)
-
-model = pickle.load(open('maas.pkl','rb'))
+@app.route("/predict",methods=["POST"])
+def predict():
+    isim=request.form.get("isim")
+    tecrube=float(request.form.get("tecrube"))
+    yazili=float(request.form.get("yazili"))
+    mulakat=float(request.form.get("mulakat"))
+    tahmin=model.predict([[tecrube,yazili,mulakat]])
+    return render_template("index.html",tahmin="Sayin, YZ tarafindan tahmin edilen maasiniz ${}".format(tahmin[0][0]))
+if __name__=="__main__":
+    app.run(debug=True)
