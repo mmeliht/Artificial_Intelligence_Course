76cb677020d19fde0d7c3f58970022fb1ad67b98
M.Melih TÜFEKCİOĞLU <melihtufekcioglu@gmail.com>
2025-08-17 10:23:45 +0300

    Preprocessing

--- day12/app.py
+++ day12/app.py
@@ -15,4 +15,21 @@ df = pd.read_excel("cars.xls")
 x = df.drop('Price' , axis= 1)
 y = df[['Price']]
 
+# Train Test Split
 x_train , x_test , y_train , y_test = train_test_split(x,y ,random_state= 42 , test_size=0.2)
+
+# Preprocessing Ön işleme
+
+preprocessor =  ColumnTransformer(
+    transformers= [
+        ("num",StandardScaler(),["Mileage","Cylinder","Liter","Doors"]),
+        ("cat",OneHotEncoder(),["Make","Model","Trim","Type"])
+    ]
+)
+
+# Model Tanımlama
+model = LinearRegression()
+
+# Pipeline
+
+
